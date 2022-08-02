89bbdab8d391e798f6692ef7e3390d7eae3e2deb
zhang937 <1379874484@qq.com>
2022-08-02 18:39:26 +0800

    assert_db

--- common/Base.py
+++ common/Base.py
@@ -1,5 +1,10 @@
+import re
+
 from config.Conf import ConfigYaml
 from utils.MysqlUtil import MysqlUtil
+from utils.AssertUtil import AssertUtil
+
+
 def get_item_by_key(obj, key, result=None):
     """
     获取接口请求响应中指定key及其内容
@@ -31,15 +36,81 @@ def get_item_by_key(obj, key, result=None):
                 result=get_item_by_key( i, key, result )
     return result[0] if isinstance( result, list ) and len( result ) == 1 else result
 
+
 def init_db(db_alias):
-    db_info =ConfigYaml().get_db_conf_info(db_alias)
+    """
+
+    :param db_alias:
+    :return:
+    """
+    db_info=ConfigYaml().get_db_conf_info( db_alias )
     host=db_info['host']
     user=db_info["user"]
     password=db_info["password"]
     database=db_info['database']
     charset=db_info['charset']
     port=db_info['port']
-    conn=MysqlUtil(host,user,password,database,charset,port)
+    conn=MysqlUtil( host, user, password, database, charset, port )
     return conn
+
+
+def assert_db(db_name, result, db_verify):
+    """
+
+    :param db_name:
+    :param result:
+    :param db_verify:
+    :return:
+    """
+    sql=init_db( db_name )
+    db_res=sql.fetchone( db_verify )
+    verify_list=list( dict( db_res ).keys() )
+    for line in verify_list:
+        res_line=result[line]
+        res_db_line=dict( db_res )[line]
+        AssertUtil().assert_body( res_line, res_db_line )
+
+
+def res_find(data, pattern_data='\${(.*)}\$'):
+    """
+
+    :param data:
+    :param pattern_data:
+    :return:
+    """
+    # pattern=re.compile( '\${(.*)}\$' )
+    pattern=re.compile( pattern_data )
+    return pattern.findall( data )
+
+
+def res_sub(data, replace, pattern_data='\${(.*)}\$'):
+    """
+
+    :param data:
+    :param replace:
+    :param pattern_data:
+    :return:
+    """
+    pattern=re.compile( pattern_data )
+    re_res=pattern.findall( data )
+    if re_res:
+        return re.sub( pattern_data, replace, data )
+    return re_res
+
+
+def params_find(headers, cookies):
+    """
+    验证请求中是否存在需要的参数。
+    :param headers:
+    :param cookies:
+    :return:
+    """
+    if "${" in headers:
+        headers=res_find( headers )
+    if "${" in cookies:
+        cookies=res_find( cookies )
+    return headers, cookies
+
+
 if __name__ == '__main__':
-    init_db('env-1')
+    print( res_sub( '{"Authorization":"JWT ${token}$"}', '333333' ) )
--- common/ExeclData.py
+++ common/ExeclData.py
@@ -3,15 +3,34 @@ from common.ExeclConfig import DataConfig
 
 
 class Data:
-    def __init__(self,testcase_file,sheet_name):
-        self.reader=ExcelReader( testcase_file, sheet_name)
+    def __init__(self, testcase_file, sheet_name):
+        self.reader=ExcelReader( testcase_file, sheet_name )
         self.is_run=DataConfig.is_run
 
     def get_run_data(self):
-        run_list=list()
-        for line in self.reader.data():
-            if str(line[self.is_run]).lower() == 'y':
-                run_list.append(line)
+        """
+        获取自动化用例
+        :return:
+        """
+        run_list=[line for line in self.reader.data() if str( line[self.is_run] ).lower() == 'y']
         return run_list
+
+    def get_case_list(self):
+        """
+        获取全部用例
+        :return:
+        """
+        all_list=[ line for line in self.reader.data()]
+        return all_list
+    def get_case_pre(self,pre):
+        """
+        根据前置条件，从全局用例中取用例。
+        :param pre:
+        :return:
+        """
+        all_list=self.get_case_list()
+        pre_case=[line for line in all_list if pre in dict[line].values()]
+        return  pre_case
+
 if __name__ == '__main__':
     aa=Data()
